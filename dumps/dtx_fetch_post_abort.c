4ab0c218695952951cd656cb60cfb10107ec3874
Jan Michalski <jan.michalski@intel.com>
2023-10-10 14:01:56 -0400

    dumps: add DTX placeholders
    Signed-off-by: Jan Michalski <jan.michalski@intel.com>

---
+++ dumps/dtx_fetch_post_abort.c
@@ -0,0 +1,8 @@
+// -f VOS510
+// vts_dtx_abort_visibility
+io_test_obj_fetch
+        vos_fetch_begin
+                // TODO
+        // TODO
+        vos_fetch_end
+                // TODO
---
+++ dumps/dtx_update_abort.c
@@ -0,0 +1,10 @@
+// -f VOS510
+// vts_dtx_abort_visibility
+vts_dtx_begin // a test-specific dtx_begin() altenative
+        // TODO
+io_test_obj_update / vos_obj_update
+        // TODO
+vts_dtx_end // a test-specific dtx_end() altenative?
+        // TODO
+vos_dtx_abort
+        // TODO
